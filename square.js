01eb2af941e456cb66bfbd620029e21edaf971d5
Giles Thomas <giles@giles.net>
2011-02-17 16:14:11 +0000

    Moved push/pop to matrix stack out of triangle/square.

--- square.js
+++ square.js
@@ -32,13 +32,11 @@
             }
             gl.useProgram(shaderProgram.program);
 
-            matrices.pushMV();
             mat4.translate(matrices.mv, [1.5, 0.0, 0.0]);
             gl.bindBuffer(gl.ARRAY_BUFFER, self.vertexPositionBuffer);
             gl.vertexAttribPointer(shaderProgram.attributes.vertexPosition, self.vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
             shaderProgram.setMatrices(matrices);
             gl.drawArrays(gl.TRIANGLE_STRIP, 0, self.vertexPositionBuffer.numItems);
-            matrices.popMV();
         };
     }
 
